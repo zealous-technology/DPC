05c0589d4310105830886240eb0bd7dab1f8ddee
STEPHEN AGER <steve_ager@hotmail.com>
2023-02-07 10:43:34 -0700

    Updated build file

--- DPC.Solution/DPC.Web/Program.cs
+++ DPC.Solution/DPC.Web/Program.cs
@@ -1,15 +1,26 @@
-var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+namespace DPC.Web
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
+            // Add services to the container.
 
-var app = builder.Build();
+            builder.Services.AddControllers();
 
-// Configure the HTTP request pipeline.
+            var app = builder.Build();
 
-app.UseAuthorization();
+            // Configure the HTTP request pipeline.
 
-app.MapControllers();
+            app.UseAuthorization();
 
-app.Run();
+            app.MapControllers();
+
+            app.Run();
+
+        }
+    }
+}
