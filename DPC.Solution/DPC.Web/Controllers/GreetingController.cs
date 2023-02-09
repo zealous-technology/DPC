0d19334131b6fd3da8f041f0d7bb0d0a1ed79b1b
STEPHEN AGER <steve_ager@hotmail.com>
2023-02-09 10:43:23 -0700

    I added a new feature

--- DPC.Solution/DPC.Web/Controllers/GreetingController.cs
+++ DPC.Solution/DPC.Web/Controllers/GreetingController.cs
@@ -28,8 +28,6 @@ namespace DPC.Web.Controllers
             _logger.LogInformation("Adding greeting with message: {Message}", greeting.Message);
 
             return Created(string.Empty, new { greeting.Message });
-
-
         }
     }
 }
