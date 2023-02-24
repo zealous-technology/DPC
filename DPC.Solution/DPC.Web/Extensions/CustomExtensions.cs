d3435dea16bedf12925dbbf0ffa1784649f0e0ca
STEPHEN AGER <steve_ager@hotmail.com>
2023-02-24 10:09:57 -0700

    added custom settings

--- DPC.Solution/DPC.Web/Extensions/CustomExtensions.cs
+++ DPC.Solution/DPC.Web/Extensions/CustomExtensions.cs
@@ -1,7 +1,18 @@
-﻿namespace DPC.Web.Extensions
+﻿using DPC.Web.Models;
+
+namespace DPC.Web.Extensions
 {
     public static class CustomExtensions
     {
-        public static T GetFirstItem<T>(this IEnumerable<T> items, Func<IEnumerable<T>, IEnumerable<T>> sort) => sort(items).First();       
+        public static T GetFirstItem<T>(this IEnumerable<T> items, Func<IEnumerable<T>, IEnumerable<T>> sort) => sort(items).First();
+
+        public static void AddCustomSettings(this IServiceCollection services, Action<CustomSettings> action)
+        {
+            var customSettings = new CustomSettings();
+
+            action?.Invoke(customSettings);
+
+            services.AddSingleton<ICustomSettings>(customSettings);
+        }
     }
 }
