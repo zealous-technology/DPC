fa9d9ec21ccbf7cc265f763f3cbc85355d6af7ed
steve-ager <50927571+steve-ager@users.noreply.github.com>
2023-02-23 17:10:05 -0700

    added generic extension method (#19)
    * added generic extension method
    
    * Delete how
    
    ---------
    
    Co-authored-by: STEPHEN AGER <steve_ager@hotmail.com>

---
+++ DPC.Solution/DPC.Web/Extensions/CustomExtensions.cs
@@ -0,0 +1,7 @@
+﻿namespace DPC.Web.Extensions
+{
+    public static class CustomExtensions
+    {
+        public static T GetFirstItem<T>(this IEnumerable<T> items, Func<IEnumerable<T>, IEnumerable<T>> sort) => sort(items).First();       
+    }
+}
