6f5bff6c3559a0c194d94fa3ac53920ae8a9fad6
Stephen Ager <50927571+steve-ager@users.noreply.github.com>
2023-07-21 14:11:31 -0600

    Update GreetingService.cs

--- DPC.Solution/DPC.Web/Services/GreetingService.cs
+++ DPC.Solution/DPC.Web/Services/GreetingService.cs
@@ -3,10 +3,10 @@
     public class GreetingService : IGreetingService
     {
     //intentinally adding a token to see if codeQL finds the token
-    //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
+    //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
 
-        string token = "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";
-        string secret = "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";
+        string token = "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";
+        string secret = "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";
 
         string bad_sql = "SELECT * FROM Users WHERE UserId = 105 OR 1=1;";
 
