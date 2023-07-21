58a517dd48edf6950cfdafab6aa2108d3eb6ff24
Stephen Ager <50927571+steve-ager@users.noreply.github.com>
2023-07-21 11:59:09 -0600

    Update GreetingService.cs

--- DPC.Solution/DPC.Web/Services/GreetingService.cs
+++ DPC.Solution/DPC.Web/Services/GreetingService.cs
@@ -4,6 +4,9 @@
     {
     //intentinally adding a token to see if codeQL finds the token
     //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
+
+        var token = "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"
+        var secret = "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"
         public string SayHelloWorld() => "Hello world!";
     }
 }
