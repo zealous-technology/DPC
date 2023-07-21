3e154069e145d20f84e8811dd175b7944fbc35ce
Stephen Ager <50927571+steve-ager@users.noreply.github.com>
2023-07-21 11:53:13 -0600

    Update GreetingService.cs

--- DPC.Solution/DPC.Web/Services/GreetingService.cs
+++ DPC.Solution/DPC.Web/Services/GreetingService.cs
@@ -2,6 +2,8 @@
 {
     public class GreetingService : IGreetingService
     {
+    //intentinally adding a token to see if codeQL finds the token
+    //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
         public string SayHelloWorld() => "Hello world!";
     }
 }
